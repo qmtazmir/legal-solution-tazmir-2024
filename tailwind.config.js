a183533a1edf030d76c38ecbab7482035cdd825c
qmtazmir <96815202+qmtazmir@users.noreply.github.com>
2024-02-12 18:45:38 +0600

    hover added

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      myfont: ["EB Garamond", "serif"],
+    },
+  },
+  plugins: [],
+};
